5f4ec24c02c6c79cb9bbf9055a16cd1c89e06e64
ktochow1 <ktochow1@binghamton.edu>
2019-02-24 23:50:18 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -17,7 +17,7 @@ def selects_oldest_bear_and_returns_name_and_age
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "SELECT MIN(age), COUNT(age) FROM bears WHERE "
+  "SELECT MIN(age) FROM bears GROUP BY age "
 end
 
 def selects_most_prominent_color_and_returns_with_count
